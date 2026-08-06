99cbf918c9fdcfd8e5a3fb1b72296302a00ae662
ss3ajel <borhen.sahbani@fsb.ucar.tn>
2026-08-07 00:22:34 +0100

    Update BGP_packet_parser.+handshakec

--- BGP_packet_parser.c
+++ BGP_packet_parser.c
@@ -19,6 +19,13 @@ struct bgp_header {
     uint16_t length;
     uint8_t type;
 } __attribute__((packed));
+struct pseudo_header {
+    uint32_t src_addr;
+    uint32_t dst_addr;
+    uint8_t  reserved;
+    uint8_t  protocol;
+    uint16_t tcp_length;
+};
 unsigned short checksum(unsigned short *buffer,int len){
     uint32_t sum=0;
     while (len > 1) {
@@ -41,13 +48,26 @@ unsigned short checksum(unsigned short *buffer,int len){
 int main(){
     char buffer[2222];
     char rbuffer[3333];
+     unsigned char broadcast_mac[6] =
+    {
+        0xff,0xff,0xff,0xff,0xff,0xff
+    };
+
     int d = socket(AF_PACKET,SOCK_RAW,0);
     if (d<0){
         perror("err1");
     }
     struct ethhdr * eth=(struct ethhdr *)buffer;
+    memcpy(eth->h_dest,broadcast_mac,6);
     struct ifreq req;
     strcpy(req.ifr_name,"wlo1");
+    ioctl(d,SIOCGIFHWADDR,&req);
+    unsigned char *my_mac =(unsigned char *)req.ifr_hwaddr.sa_data;
+    memcpy(eth->h_source,my_mac,6);
+    eth->h_proto = htons(ETH_P_IP);
+
+    ioctl(d,SIOCGIFINDEX,&req);
+    int ifindex = req.ifr_ifindex;
     ioctl(d,SIOCGIFADDR,&req);
     struct sockaddr_in *ip=(struct sockaddr_in*)&req.ifr_addr;
     uint32_t myip=ip->sin_addr.s_addr;
@@ -79,10 +99,10 @@ int main(){
     ipn->protocol = IPPROTO_TCP;
     ipn->saddr = myip;
     struct tcphdr *tcp=(struct tcphdr *)(buffer +sizeof(struct ethhdr)+sizeof(struct iphdr));
-    tcp->source=htons(179);
-    tcp->dest=
-    tcp->seq=htons(0);
-    tcp->ack_seq=htons(0);
+    tcp->source=htons(54321);
+    tcp->dest=htons(179);
+    tcp->seq=htonl(0);
+    tcp->ack_seq=htonl(0);
     tcp->doff=5;
     tcp->syn=1;
     //tcp->ack
@@ -91,24 +111,78 @@ int main(){
     //tcp->psh
     //tcp->urg
     tcp->window=htons(65535);
-    //tcp->check
-    //tcp->urg_ptr*/
+
+
     struct sockaddr_ll device;
+    memset(&device,0,sizeof(device));
+
+    device.sll_family = AF_PACKET;
+
+    device.sll_ifindex = ifindex;
+
+    device.sll_halen = ETH_ALEN;
     int binded=bind(d,(struct sockaddr*)&device,sizeof(device));
+
     if (binded<0){
        perror("not binded");
        return 1;
     }
     printf("Scanning from %u to %u (%d addresses)\n", net_h, brd_h, brd_h - net_h);//stylish
     
+    
     for(uint32_t i=net_h+1;i<brd_h;i++){
+
         ipn->daddr = htonl(i);
          ipn->check = 0;
          ipn->check = checksum((unsigned short *)ipn,sizeof(struct iphdr));
-         tcp->check=checksum(unsigned short*)tcp +sizeof(struct tcphdr);
-        sendto(d,buffer,sizeof(struct ethhdr) + sizeof(struct iphdr)+ sizeof(struct icmphdr), 0, (struct sockaddr *)&device, sizeof(device));
-        recvfrom(d, rbuffer, sizeof(rbuffer), 0, NULL, NULL);
+         tcp->check=0;
+         char pbuffer[3333];
+         struct pseudo_header *pseudo=(struct pseudo_header*)(pbuffer);
+        pseudo->src_addr = ipn->saddr;
+       pseudo->dst_addr = ipn->daddr;
+         pseudo->protocol = IPPROTO_TCP;
+        pseudo->tcp_length = htons(sizeof(struct tcphdr) + sizeof(struct bgp_header));
         
-   
+         
+        uint8_t *payload = (uint8_t *)tcp + tcp->doff * 4;
+         tcp=(struct tcphdr*)(pbuffer+sizeof(struct pseudo_header));
+
+
+         tcp->check=checksum((unsigned short*)tcp ,sizeof(pseudo));
+
+        sendto(d,buffer,sizeof(struct ethhdr) + sizeof(struct iphdr)+ sizeof(struct tcphdr), 0, (struct sockaddr *)&device, sizeof(device));
+        struct sockaddr_ll receiver;
+         receiver.sll_family=AF_PACKET;
+         receiver.sll_ifindex = ifindex;
+         receiver.sll_halen = ETH_ALEN;
+         socklen_t len=sizeof( receiver);
+         
+
+         int rec_len=recvfrom(d,rbuffer,sizeof(rbuffer),0,(struct sockaddr*)&receiver,&len);
+         if(rec_len<sizeof(struct ethhdr)) {continue;}//14 bytes}
+        struct ethhdr *reth=(struct ethhdr*)rbuffer;
+        
+       
+        if (ntohs(reth->h_proto) != 0x0800) { continue; }
+        if(rec_len<sizeof(struct ethhdr )+sizeof(struct iphdr)){continue;}
+        struct iphdr *rip=(struct iphdr*) (rbuffer+sizeof(struct ethhdr));//+20 bytes
+        
+        if (rip->protocol != IPPROTO_TCP) { continue; } // 8bytes
+        size_t ip_hd_len=rip->ihl*5;
+       
+        if (ip_hd_len < 20 || rec_len < sizeof(struct ethhdr) + ip_hd_len + sizeof(struct tcphdr)) { 
+        continue; 
 }
+        struct tcphdr *rtcp=(struct tcphdr*)(rbuffer+sizeof(struct ethhdr )+ip_hd_len);
+
+         uint8_t *payload = (uint8_t *)rtcp + rtcp->doff * 4;
+         if(rtcp->ack&&rtcp->syn){
+            //open
+         }else (rtcp->rst){
+            //close
+         }
+         struct bgp_header *bgp = (struct bgp_header *)payload;
+         // if (port == 179 && has_payload) parse_bgp(payload);
+}
+
 }
